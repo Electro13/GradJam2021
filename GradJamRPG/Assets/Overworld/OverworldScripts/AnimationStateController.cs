df63e480f008e026345a7398db1fa81c39cea3ae
MilesFairfield <mewf@cogeco.net>
2021-05-22 01:36:39 -0400

    Overworld Walk/Run
    - Full implementation of Idle, Walk, and Run anims
    
    - Next steps, Sword / Attack anims. overworld environment, breakables and collectables.

---
+++ GradJamRPG/Assets/Overworld/OverworldScripts/AnimationStateController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+
+    public Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+      
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        anim.SetFloat("speed", Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs((Input.GetAxis("Horizontal"))) );
+
+
+
+
+    }
+}
